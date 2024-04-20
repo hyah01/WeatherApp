ac8fbbcd941c56a9b0843150bde28137e1ce5621
Hy <hyhuynh222@gmail.com>
2024-04-19 20:25:12 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+// defining the server port
+const port = 5000
+
+// initializing installed dependencies
+const express = require('express')
+require('dotenv').config()
+const axios = require('axios')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+// listening for port 5000
+app.listen(5000, ()=> console.log(`Server is running on ${port}` ))
+
+
+
+// const options = {method: 'GET', headers: {accept: 'application/json'}};
+//     const API = process.env.REACT_APP_API_KEY;
+
+//     fetch(`https://swapi.dev/api/people/1/`, options)
+//       .then(response => response.json())
+//       .then(response => console.log(response))
+//       .catch(err => console.error(err));
+
+// API request
+app.get('/', (req,res)=>{    
+    
+    const API = process.env.REACT_APP_API_KEY;
+    const location = req.headers.location.replace(" ", "%20");
+
+    const options = {
+        method: 'GET',
+        url: `https://api.tomorrow.io/v4/weather/forecast?location=${location}&timesteps=1h%2C1d&units=imperial&apikey=${API}`,
+        //url: `https://swapi.dev/api/people/${location}/`,
+        headers: {
+            accept: 'application/json'
+        }
+   };
+   
+    axios.request(options).then(function (response) {
+        res.json(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
+)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import {useState, useEffect, useCallback} from "react"
+import weatherData from './weather_test.json'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [data, setData] = useState(null)
+  const [text, setText] = useState("")
+  const [daily, setDaily] = useState(null)
+  const [hourly, Sethourly] = useState(null)
+
+
+
+
+  useEffect(()=>{
+    
+  }, [])
+
+
+  const onPress = async (e) => {
+    e.preventDefault();
+    try {
+      setData(weatherData.location);
+      setDaily(weatherData.timelines.daily)
+      Sethourly(weatherData.timelines.hourly)
+      console.log(weatherData.location)
+
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+
+      const options = {
+        method: 'GET',
+        url: "http://localhost:5000",
+        headers: {
+          location: text
+        },
+      }
+
+      const result = await axios.request(options)
+      setData(result.data.location);
+      setDaily(result.data.timelines.daily);
+      Sethourly(result.data.timelines.hourly);
+      console.log(result.data.timelines.daily)
+
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+
+  return (
+    <main className="App">
+      <h1>Weather</h1>
+      <input type="text" id="location" name='location'onChange={(e) => setText(e.target.value)}/>
+      <button id="post" onClick={onPress}> Submit</button>
+      
+      {
+        data && [data].map(post => {
+          return(
+            <div>
+              <h1>{post.name}</h1>
+            </div>
+          )
+      })}
+      {
+        daily && daily.map(post => {
+          return (
+            <div key={post.time}>
+              <h1>{post.time.slice(5,10).replace("-","/")}</h1>
+              <h3>Avg. Temp</h3>
+              <h2>{post.values.temperatureAvg}</h2>
+            </div>
+          )
+        })
+      }
+    </main>
+  )
+}
 export default App;
+
